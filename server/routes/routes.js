69ac711d95038fb5d06ebc5dc0bd1614e8a24bf9
krai54rus <krai54rus@gmail.com>
2021-06-22 23:54:28 +0700

    qwe

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,16 +5,4 @@ module.exports = function(app,db) {
   require('./catalog')(app,db);
   require('./constructor')(app,db);
   require('./personal')(app,db);
-  //const beerRouter = app.Router;
-  // app.get('/beer', function(req,res){
-  //   const collection = db.db("portal").collection("beer");
-  //   collection.find({}).toArray(function(err, beers){
-  //       if(err) return console.log(err);
-  //       res.send(beers)
-  //   });
-  // });
-  // app.post('/', (req, res) => {
-  //   console.log(req.body)
-  //   res.send('Hello')
-  // });
 };
